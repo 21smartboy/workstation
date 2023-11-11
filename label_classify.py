2c1d7fc5748a2223929eeaca54de1d2d7eef1117
iceman <3201127883@qq.com>
2023-11-11 21:07:26 +0800

    一个标签归类代码

---
+++ label_classify.py
@@ -0,0 +1,8 @@
+def classify(target_lab,subset_label,all_label)
+    last_lab=[]
+    for item in all_label:
+        if item in subset_label
+            last_lab.append(target_lab)
+        else:
+            last_lab.append(item)
+    return last_lab
